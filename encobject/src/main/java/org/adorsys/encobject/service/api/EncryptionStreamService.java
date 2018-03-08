5b6e5875cc5a32049093ec01d37b12d77d65394e
pspex <git@electronicpeter.de>
2018-03-08 17:31:31 +0100

    erster cipher test erfolgreich, aber mit Einbindung von plooh-core

---
+++ encobject/src/main/java/org/adorsys/encobject/service/api/EncryptionStreamService.java
@@ -0,0 +1,13 @@
+package org.adorsys.encobject.service.api;
+
+import org.adorsys.encobject.types.KeyID;
+
+import java.io.InputStream;
+
+/**
+ * Created by peter on 07.03.18 at 09:35.
+ */
+public interface EncryptionStreamService {
+    InputStream getEncryptedInputStream(InputStream inputStream, KeySource keySource, KeyID keyID, Boolean compress);
+    InputStream getDecryptedInputStream(InputStream inputStream, KeySource keySource);
+}
---
+++ filesystemstoreconnection/src/test/java/org/adorsys/encobject/service/EncryptionTest.java
@@ -0,0 +1,100 @@
+package org.adorsys.encobject.service;
+
+import com.google.protobuf.ByteString;
+import junit.framework.Assert;
+import org.adorsys.cryptoutils.exceptions.BaseExceptionHandler;
+import org.adorsys.cryptoutils.utils.HexUtil;
+import org.adorsys.encobject.service.impl.generator.SecretKeyGeneratorImpl;
+import org.adorsys.encobject.types.KeyID;
+import org.adorsys.jkeygen.keystore.SecretKeyData;
+import org.adorsys.plooh.core.encrypt.SecretKeyAlgoFactory;
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.SecretKey;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.UUID;
+
+/**
+ * Created by peter on 08.03.18 at 16:13.
+ */
+public class EncryptionTest {
+    private final static Logger LOGGER = LoggerFactory.getLogger(EncryptionTest.class);
+
+    @Test
+    public void a() {
+        try {
+            KeyIDWithKey t = createDocumentKeyIdWithKey();
+            LOGGER.info(t.toString());
+
+            byte[] decrypted = "Der Affe ist ein Affe und das bleibt auch so".getBytes();
+            InputStream decryptedStream = new ByteArrayInputStream(decrypted);
+            CipherInputStream encryptionStream = SecretKeyAlgoFactory.AES_SECRET_KEY_ALGO.createCipherInputStream(t.getSecretKey().getEncoded(), decryptedStream, Cipher.ENCRYPT_MODE);
+            byte[] encrypted = IOUtils.toByteArray(encryptionStream);
+
+            InputStream encryptedInputStream = new ByteArrayInputStream(encrypted);
+            CipherInputStream decryptionStream = SecretKeyAlgoFactory.AES_SECRET_KEY_ALGO.createCipherInputStream(t.getSecretKey().getEncoded(), encryptedInputStream, Cipher.DECRYPT_MODE);
+            byte[] redecrypted = IOUtils.toByteArray(decryptionStream);
+
+            LOGGER.info("  decrypted : " + HexUtil.convertBytesToHexString(decrypted));
+            LOGGER.info("  encrypted : " + HexUtil.convertBytesToHexString(encrypted));
+            LOGGER.info("redecrypted : " + HexUtil.convertBytesToHexString(redecrypted));
+
+            Assert.assertTrue(Arrays.equals(decrypted, redecrypted));
+        } catch (Exception e) {
+            throw BaseExceptionHandler.handle(e);
+        }
+    }
+
+    public KeyIDWithKey createDocumentKeyIdWithKey() {
+        // Eine zufällige DocumentKeyID erzeugen
+        KeyID documentKeyID = new KeyID("DK" + UUID.randomUUID().toString());
+
+        // Für die DocumentKeyID einen DocumentKey erzeugen
+        SecretKeyGeneratorImpl secretKeyGenerator = new SecretKeyGeneratorImpl("AES", 256);
+        SecretKeyData secretKeyData = secretKeyGenerator.generate(documentKeyID.getValue(), null);
+        SecretKey documentKey = secretKeyData.getSecretKey();
+        return new KeyIDWithKey(documentKeyID, documentKey);
+    }
+
+    ByteString getSecretKeyAsByteString(SecretKey key) {
+        return ByteString.copyFrom(key.getEncoded());
+    }
+
+    private static class KeyIDWithKey {
+        private final KeyID keyID;
+        private final SecretKey secretKey;
+
+        public KeyIDWithKey(KeyID keyID, SecretKey secretKey) {
+            this.keyID = keyID;
+            this.secretKey = secretKey;
+        }
+
+        public KeyID getKeyID() {
+            return keyID;
+        }
+
+        public SecretKey getSecretKey() {
+            return secretKey;
+        }
+
+        @Override
+        public String toString() {
+            return "KeyIDWithKey{" +
+                    "\n               keyID = " + keyID.getValue() +
+                    "\n   secretKey.encoded = " + HexUtil.convertBytesToHexString(secretKey.getEncoded()) +
+                    "\n secretKey.algorithm = " + secretKey.getAlgorithm() +
+                    "\n  sesecretKey.format = " + secretKey.getFormat() +
+                    "\n" +
+                    '}';
+        }
+    }
+
+
+}

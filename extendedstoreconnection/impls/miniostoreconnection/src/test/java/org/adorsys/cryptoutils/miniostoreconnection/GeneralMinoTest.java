e012fbd49de54a86412ce0e47920f84e70dba190
pspex <git@electronicpeter.de>
2018-03-22 22:43:39 +0100

    DOC-16 Minio mit nur einem Folder

--- extendedstoreconnection/impls/miniostoreconnection/src/test/java/org/adorsys/cryptoutils/miniostoreconnection/GeneralMinoTest.java
+++ extendedstoreconnection/impls/miniostoreconnection/src/test/java/org/adorsys/cryptoutils/miniostoreconnection/GeneralMinoTest.java
@@ -1,9 +1,11 @@
 package org.adorsys.cryptoutils.miniostoreconnection;
 
 import io.minio.MinioClient;
-import junit.framework.Assert;
 import org.adorsys.cryptoutils.exceptions.BaseExceptionHandler;
+import org.adorsys.encobject.complextypes.BucketDirectory;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.URL;
 
@@ -11,24 +13,51 @@ import java.net.URL;
  * Created by peter on 18.03.18 at 20:02.
  */
 public class GeneralMinoTest {
-    // @Test
-    public void a() {
+    private final static Logger LOGGER = LoggerFactory.getLogger(GeneralMinoTest.class);
+    private MinioAccessKey accessKey = new MinioAccessKey("9J8I2EAWUNRVRLXIVV5B");
+    private MinioSecretKey secretKey = new MinioSecretKey("wJKOJNWiQVBkzIipinJlG8k6iCFlSlES1c9mo2jI");
+    private URL url = getUrl("http://localhost:9000");
+
+    private static URL getUrl(String url) {
         try {
-            String accessKey = "7IVCW3EES6A5L9YV5VO8";
-            String secretKey= "6aTPgIRhyfH5sHdmff69Mizjiefudf0mFtuwNXN7";
-            URL url = new URL("http://localhost:9000");
-            MinioClient minioClient = new MinioClient(url, accessKey, secretKey);
-
-            String bucket = "affe";
-            Assert.assertFalse(minioClient.bucketExists(bucket));
-            minioClient.makeBucket(bucket);
-            Assert.assertTrue(minioClient.bucketExists(bucket));
-            minioClient.removeBucket(bucket);
-            Assert.assertFalse(minioClient.bucketExists(bucket));
-
+            return new URL(url);
         } catch (Exception e) {
             throw BaseExceptionHandler.handle(e);
         }
-
     }
+
+    // @Test
+    public void a() {
+        try {
+            MinioClient minioClient = new MinioClient(url, accessKey.getValue(), secretKey.getValue());
+
+
+            BucketDirectory rootBucket = new BucketDirectory("org.adorsys.cryptoutils");
+            String CONTAINER_FILE = ".container.marker.file";
+
+            LOGGER.info("list now " + rootBucket);
+            minioClient.listObjects(rootBucket.getObjectHandle().getContainer(), ".*/" + CONTAINER_FILE, true).forEach(
+                    el -> {
+                        try {
+                            LOGGER.info("found " + el.get().objectName());
+
+                        } catch (Exception e) {
+                            throw BaseExceptionHandler.handle(e);
+                        }
+                    });
+        } catch (Exception e) {
+            throw BaseExceptionHandler.handle(e);
+        }
+    }
+
+    // @Test
+    public void b() {
+        new MinioExtendedStoreConnection(url, accessKey, secretKey).showDatabase();
+    }
+
+    // @Test
+    public void c() {
+        new MinioExtendedStoreConnection(url, accessKey, secretKey).cleanDatabase();
+    }
+
 }
